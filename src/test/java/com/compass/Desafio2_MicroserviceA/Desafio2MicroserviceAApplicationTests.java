179ee624debc12651d0b2513781c044ab8924cbc
Henrique Jornada <jornadahenrique12@gmail.com>
2025-02-05 15:27:20 -0300

    [Feature] | Criando MicroServiceA

---
+++ src/test/java/com/compass/Desafio2_MicroserviceA/Desafio2MicroserviceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.Desafio2_MicroserviceA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio2MicroserviceAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
