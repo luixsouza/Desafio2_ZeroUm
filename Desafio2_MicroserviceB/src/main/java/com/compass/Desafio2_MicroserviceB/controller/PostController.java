e3c43063385cf34f8c3ecf830b61eab14d17b9ad
Luis Eduardo <luixst1@gmail.com>
2025-02-05 16:10:21 -0300

    [feature] | Refatorando endpoints na PostController baseado no fluxo

--- Desafio2_MicroserviceB/src/main/java/com/compass/Desafio2_MicroserviceB/controller/PostController.java
+++ Desafio2_MicroserviceB/src/main/java/com/compass/Desafio2_MicroserviceB/controller/PostController.java
@@ -3,36 +3,55 @@ package com.compass.Desafio2_MicroserviceB.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.compass.Desafio2_MicroserviceB.dto.CommentDTO;
 import com.compass.Desafio2_MicroserviceB.dto.PostDTO;
 import com.compass.Desafio2_MicroserviceB.service.PostService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
-    @GetMapping
+    @PostMapping("/sync-data")
+    public void syncData() {
+        postService.syncData();
+    }
+
+    @GetMapping("/posts")
     public ResponseEntity<List<PostDTO>> getAllPosts() {
         return ResponseEntity.ok(postService.getAllPosts());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/posts/{id}")
     public ResponseEntity<PostDTO> getPostById(@PathVariable Long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
-    @GetMapping("/{id}/comments")
-    public ResponseEntity<List<CommentDTO>> getCommentsByPostId(@PathVariable Long id) {
-        return ResponseEntity.ok(postService.getCommentsByPostId(id));
+    @PostMapping("/posts")
+    public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO postDTO) {
+        return ResponseEntity.ok(postService.createPost(postDTO));
+    }
+
+    @PutMapping("/posts/{id}")
+    public ResponseEntity<PostDTO> updatePost(@PathVariable Long id, @RequestBody PostDTO postDTO) {
+        return ResponseEntity.ok(postService.updatePost(id, postDTO));
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+        return ResponseEntity.noContent().build();
     }
 }
